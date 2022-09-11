9377579c3fefe3ef9d9c068ecab18a97a95df145
Ashish Mangla <ashmangl@WKMZT0327482.global.publicisgroupe.net>
2022-09-11 11:43:58 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment, useState } from "react";
+
+const data = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+  { id: 4, name: "anna" },
+];
+
+
+
+
+function App() {
+  const [people, setPeople] = useState(data);
+  const handleClearAll = () =>{
+    setPeople([]);
+  }
+  const handleSingleRemove = (id) =>{
+console.log(id);
+let newPeople = people.filter((person)=> person.id !== id);
+console.log(newPeople);
+setPeople(newPeople);
+  }
+  return (
+    <div className="App">
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <Fragment key={name}>
+            <h4>{name}</h4>
+            <button className="" onClick={ () => handleSingleRemove(id)}>Remove Item</button>
+          </Fragment>
+        );
+      })}
+      <button className="" onClick={handleClearAll}>
+        Clear All
+      </button>
+    </div>
+  );
+}
+
+export default App;
