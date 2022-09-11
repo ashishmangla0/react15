375a2b172710257759773f99a41dd598e20bd7a3
Ashish Mangla <ashmangl@WKMZT0327482.global.publicisgroupe.net>
2022-09-11 12:21:38 +0530

    add object changes

--- src/App.js
+++ src/App.js
@@ -9,34 +9,66 @@ const data = [
   { id: 4, name: "anna" },
 ];
 
-
-
-
+const initalValues = {
+  name: "peter",
+  age: 24,
+  message: "random message",
+};
 function App() {
   const [people, setPeople] = useState(data);
-  const handleClearAll = () =>{
+  const [person, setPerson] = useState(initalValues);
+  const handleClearAll = () => {
     setPeople([]);
-  }
-  const handleSingleRemove = (id) =>{
-console.log(id);
-let newPeople = people.filter((person)=> person.id !== id);
-console.log(newPeople);
-setPeople(newPeople);
-  }
+  };
+  const handleSingleRemove = (id) => {
+    console.log(id);
+    let newPeople = people.filter((person) => person.id !== id);
+    console.log(newPeople);
+    setPeople(newPeople);
+  };
+
+
+const changeMessage = () =>{
+  setPerson({...person, message: 'hello World' });
+}
+
+
   return (
     <div className="App">
+      <div>
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={name} className={'item'}>
+          <div key={name} className={"item"}>
             <h4>{name}</h4>
-            <button className="" onClick={ () => handleSingleRemove(id)}>Remove Item</button>
+            <button className="" onClick={() => handleSingleRemove(id)}>
+              Remove Item
+            </button>
           </div>
         );
       })}
       <button onClick={handleClearAll} className="btn">
         Clear All
       </button>
+      </div>
+      <div>
+        <h2>
+          object change
+        </h2>
+        <h3>
+          {person.name}
+        </h3>
+        <h3>
+          {person.age}
+        </h3>
+        <h3>
+          {person.message}
+        </h3>
+        <button className="btn" onClick={changeMessage} >
+          change message
+        </button>
+      </div>
+      
     </div>
   );
 }
