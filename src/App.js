879eeca4ac538a8d4a5cda0e372f7100164e8949
Ashish Mangla <ashmangl@WKMZT0327482.global.publicisgroupe.net>
2022-09-14 11:10:05 +0530

    add new code for birthday

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Fragment, useState } from "react";
+import Birthday from "./Birthday";
 
 function App() {
-  const initalValue = 0;
-
-  const [value, setValue] = useState(initalValue);
-
-  const handleInc = () => {
-    setValue(value + 1);
-  };
-  const handleDec = () => {
-    console.log("i am dec");
-    setValue(value - 1);
-  };
-  const handleReset = () => {
-    setValue(initalValue);
-  };
-  return (
-    <section className="App">
-      <h2> Counter</h2>
-      <h3>{value}</h3>
-      <button className="btn" onClick={handleDec}>
-        decrease
-      </button>
-      <button className="btn" onClick={handleReset}>
-        reset
-      </button>
-      <button className="btn" onClick={handleInc}>
-        increase
-      </button>
-    </section>
-  );
+ return(
+  <Birthday/>
+ )
 }
 
 export default App;
---
+++ src/Birthday/Birthday.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+console.log(data);
+
+const Birthday = () => {
+
+const [people,setPeople] =  useState(data);
+
+const handleClear = () =>{
+setPeople([]);
+}
+
+
+  return (
+    <>
+
+    <section className="container">
+      <h3>
+        {people.length} Birthday today
+
+      </h3>
+      <List people={people} />
+      <button onClick={handleClear}> clear all</button>
+    </section>
+      
+    </>
+  );
+};
+
+export default Birthday;
