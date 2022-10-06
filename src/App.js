2f9b327ddef14b5f83e805af6e931f05577a4766
Ashish Mangla <ashmangl@WKMZT0327482.global.publicisgroupe.net>
2022-10-06 12:10:41 +0530

    add accordian

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from "./logo.svg";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import CardShimer from "./CardShimer";
 // import Birthday from "./Birthday";
 // import Tours from "./Tours";
-import Reviews from "./Reviews";
+// import Reviews from "./Reviews";
+// import Images from "./Images";
+// import Accordion from "./Accordion";
+
+
+
 function App() {
   return (
     <div className="App">
       {/* <Project/> */}
       {/* <Birthday/> */}
       {/* <Tours/> */}
-      <Reviews/>
+      {/* <Reviews/> */}
+      {/* <Accordion/> */}
+      <CardShimer/>
     </div>
   );
 }
